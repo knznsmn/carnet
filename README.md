7641ae8332266b1d00138d05b74ed7213913c858
Julius Cinco Cesar <knznsmn@icloud.com>
2025-03-20 07:35:26 +0400

    Housekeeping

--- README.md
+++
@@ -1,5 +0,0 @@
-# carnet
-
-## Deuxieme Cerveau
-
-Second Brain on the Cloud
--- app/(home)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from 'react';
-import HomeLayout from './homelayout';
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout>{children}</HomeLayout>;
-}
