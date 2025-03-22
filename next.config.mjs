7362cf2bd7fa207bb55bad182a1423ef0f75d443
Julius Cinco Cesar <knznsmn@knznsmn.com>
2025-03-22 21:23:44 +0400

    Set up fresh Nextra

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  latex: true,
+  search: {
+    codeblocks: false
+  },
+  contentDirBasePath: '/docs'
+})
+
+export default withNextra({
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+})
